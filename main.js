db5269a222519587196403e330572a72191c9f76
santhosh9981 <85357586+santhosh9981@users.noreply.github.com>
2021-06-12 17:58:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+function one() {
+    widow.alert(Cricket is my favorite hobby and a famous sport too.It makes me feel healthy, fit and a stronger person.It makes me feel confident and proud of what I am.I started playing cricket when I was a of 10 years old.this game with my small brother in my home.)
+}
